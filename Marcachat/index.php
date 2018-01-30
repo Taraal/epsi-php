951a375c6b0e16930d35efdd94f1cb0db074b835
Sylouan Corfa <35556831+Taraal@users.noreply.github.com>
2018-01-30 12:21:25 +0100

    Add files via upload

---
+++ Marcachat/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>MARCACHAT</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet">   
+</head>
+
+<body>
+	<div class="login">
+		<div class="login-screen">
+                   
+                    <div class ='logo' id='marcassin'>
+                        <img src='medias/marcassin.png' alt="Le logo marcassin">
+                    </div>
+
+                    <div class='logo' id='chat'>
+                        <img src="medias/Chat.png" alt="Le logo du chat">
+                    </div>
+                    
+                    <div class="app-title">
+			<h1>Marca'chat</h1>
+                    </div>
+
+                    <div class="login-form"> 
+			<div class="control-group">
+			<input type="text" class="login-field" value="" placeholder="Nom d'utilisateur" id="login-name">
+			<label class="login-field-icon fui-user" for="login-name"></label>
+			</div>
+
+			<div class="control-group">
+			<input type="password" class="login-field" value="" placeholder="Mot de passe" id="login-pass">
+			<label class="login-field-icon fui-lock" for="login-pass"></label>
+			</div>
+
+                        <a class="btn btn-primary btn-large btn-block" href="#">Se connecter</a>
+                    </div>
+		</div>
+	</div>
+</body>
+
+</html>
