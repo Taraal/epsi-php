59847064257128ed734016acd8459ac75edb88a4
SimonRethore <srethore.pro@gmail.com>
2018-03-15 10:53:37 +0100

    Add files via upload

---
+++ Importation_CSV.php
@@ -0,0 +1,29 @@
+<?php include_once 'Bootstrap.php'; ?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/csv.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+
+        <div class="container">
+
+            <h1>Veuillez choisir le fichier .csv à importer</h1>
+            
+            <br>
+
+            <form method="post" enctype="multipart/form-data" action="import.php">
+
+                <input name="userfile" type="file" value="table"><br>
+                <input class="btn btn-default" name="submit" type="submit" value="Importer">
+
+            </form>
+
+        </div>
+
+    </body>
+</html>
